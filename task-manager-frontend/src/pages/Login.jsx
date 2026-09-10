b7ac44024f0bdafe6407cc7331f1d73ab155917f
Ayan Pratap <ayanpratap5@gmail.com>
2026-09-10 16:45:36 +0530

    added UI

--- task-manager-frontend/src/pages/Login.jsx
+++ task-manager-frontend/src/pages/Login.jsx
@@ -46,7 +46,7 @@ function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-400">
+    <div className="min-h-screen flex items-center justify-center bg-[url('../../../task-manager-frontend/public/LoginBG.png')] bg-cover bg-center">
       <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
         <h1 className="text-3xl font-bold text-center mb-6">Task Manager</h1>
 
