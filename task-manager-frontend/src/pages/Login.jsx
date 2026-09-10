527939f9012549dadb7ffedb0a64530ce32be1e9
Ayan Pratap <ayanpratap5@gmail.com>
2026-09-10 17:04:27 +0530

    Updated Login UI

--- task-manager-frontend/src/pages/Login.jsx
+++ task-manager-frontend/src/pages/Login.jsx
@@ -46,53 +46,56 @@ function Login() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-[url('../../../task-manager-frontend/public/LoginBG.png')] bg-cover bg-center">
-      <div className="bg-white p-8 rounded-xl shadow-md w-full max-w-md">
-        <h1 className="text-3xl font-bold text-center mb-6">Task Manager</h1>
+    <div className="relative w-full h-screen overflow-hidden">
+      <div className="absolute inset-0 bg-[url('/LoginBG.png')] bg-cover bg-center backdrop-blur-md"></div>
+      <div className="min-h-screen flex items-center justify-center relative z-10 ">
+        <div className="bg-white/20 p-8 rounded-xl shadow-md w-full max-w-md border shadow-black/50" >
+          <h1 className="text-3xl font-bold text-center mb-6">Task Flow</h1>
 
-        <form onSubmit={handleSubmit} className="space-y-5">
-          <div>
-            <label className="block mb-2 font-medium">Email</label>
+          <form onSubmit={handleSubmit} className="space-y-5">
+            <div>
+              <label className="block mb-2 font-medium">Email</label>
 
-            <input
-              type="email"
-              name="email"
-              value={formData.email}
-              onChange={handleChange}
-              placeholder="Enter your email"
-              className="w-full border p-3 rounded-lg"
-            />
-          </div>
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="Enter your email"
+                className="w-full border p-3 rounded-lg"
+              />
+            </div>
 
-          <div>
-            <label className="block mb-2 font-medium">Password</label>
+            <div>
+              <label className="block mb-2 font-medium">Password</label>
 
-            <input
-              type="password"
-              name="password"
-              value={formData.password}
-              onChange={handleChange}
-              placeholder="Enter password"
-              className="w-full border p-3 rounded-lg"
-            />
-          </div>
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="Enter password"
+                className="w-full border p-3 rounded-lg"
+              />
+            </div>
 
-          <button
-            disabled={loading}
-            className="w-full bg-black text-white py-3 rounded-lg hover:bg-gray-800"
-          >
-            {loading ? "Logging in..." : "Login"}
-          </button>
-        </form>
-        <p className="text-center mt-5">
-          Don't have an account?
-          <span
-            onClick={() => navigate("/register")}
-            className="ml-2 cursor-pointer font-semibold"
-          >
-            Register
-          </span>
-        </p>
+            <button
+              disabled={loading}
+              className="w-full bg-black/70 text-white py-3 rounded-lg hover:bg-black/90 hover:transition-all duration-300 scale-102"
+            >
+              {loading ? "Logging in..." : "Login"}
+            </button>
+          </form>
+          <p className="text-center mt-5">
+            Don't have an account?
+            <span
+              onClick={() => navigate("/register")}
+              className="ml-2 cursor-pointer font-semibold"
+            >
+              Register
+            </span>
+          </p>
+        </div>
       </div>
     </div>
   );
